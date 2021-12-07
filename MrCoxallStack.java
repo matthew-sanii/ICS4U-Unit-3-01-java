0f3f399695f315935fbf6b24b5733010ae8b6cf3
matthew-sanii <60710943+matthew-sanii@users.noreply.github.com>
2021-12-07 21:21:02 +0000

    edited both

--- MrCoxallStack.java
+++ MrCoxallStack.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class MrCoxallStack {
   private ArrayList<Integer> theStack = new ArrayList<Integer>();
-
+  // push takes the value given and adds to stack.
   public void push(final int pushValue) {
     System.out.println("adding " + pushValue + " to stack.");
     theStack.add(pushValue);
   }
-
+  // showStack shows the created stack to user.
   public void showStack() {
     System.out.println("The stacklist.");
     for (Integer aSingleElement : theStack) {
